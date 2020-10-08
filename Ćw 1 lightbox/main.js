3f28fc67f6cc8bd61d3f8a4c4784f9aba87c29e3
Damian Orzechowski <damianmisiek110@gmail.com>
2020-10-08 14:13:13 +0200

    podstawa

---
+++ Ćw 1 lightbox/main.js
@@ -0,0 +1,8 @@
+const f  = document.querySelector('#f');
+f.addEventListner('click',showLightbox)
+
+function showLightbox() {
+    const lightbox = document.querySelector('.lightbox');
+    lightbox.style.transform = 'scale(1)'; 
+    console.log('showlight');
+}
