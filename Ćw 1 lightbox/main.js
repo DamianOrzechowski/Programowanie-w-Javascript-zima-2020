6f560ba6afc281e065312c7dfc890d59b624583e
Damian Orzechowski <damianmisiek110@gmail.com>
2020-10-13 14:14:14 +0200

    poprawione

--- Ćw 1 lightbox/main.js
+++ Ćw 1 lightbox/main.js
@@ -1,25 +1,35 @@
 const images = document.querySelectorAll('.grid img')
 const lightbox = document.querySelector('.lightbox')
-const close = document.querySelector('#close')
-const prev = document.querySelector('#prev')
-const next = document.querySelector('#next')
 
+const prev = document.querySelector('#prev')// przycisk poprzednie zdjecie
+const next = document.querySelector('#next')// przycisk nastepne zdjecie
+const close = document.querySelector('#close')// przycisk zamykanie lightboxa
+let prevEl// nastepny element danego zdjecia
+let nextEl// poprzedni element danego zjecia
 
-
-
-close.addEventListener('click', hidelightbox)
-function hidelightbox ()
-{
-    lightbox.classList.remove('visible')
+close.addEventListener('click', hideLightbox)// zamykanie lighboxa
+function hideLightbox () {
+  lightbox.classList.remove('visible')
 }
+
 for (let index = 0; index < images.length; index++) {
   const img = images[index]
   img.addEventListener('click', showLightbox)
 }
 
-function showLightbox (ev) {
-    
-    const lighboxImg = document.querySelector('.lightbox img')
-    lighboxImg.src = ev.target.src
-    lightbox.classList.add('visible')
-  }
+function showLightbox (ev) { // otwieranie lightboxa
+  const lightboxImg = document.querySelector('.lightbox img')
+  lightboxImg.src = ev.target.src
+  lightbox.classList.add('visible')
+  prevEl = ev.target.previousElementSibling
+  nextEl = ev.target.nextElementSibling
+}
+next.addEventListener('click', function () { // przwijanie zdjec do przodu
+  hideLightbox()
+  nextEl ? nextEl.click() : images[0].click()// sprawdzenie czy obecne zdjecie jest ostatnim  w kolejnosci, jesli tak to nastepuje zawiniecie
+})
+
+prev.addEventListener('click', function () {
+  hideLightbox()
+  prevEl ? prevEl.click() : images[images.length - 1].click()// sprawdzenie czy obecne zdjecie jest pierwszym  w kolejnosci, jesli tak to nastepuje zawiniecie
+})
