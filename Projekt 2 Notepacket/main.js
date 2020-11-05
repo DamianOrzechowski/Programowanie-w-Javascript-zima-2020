cb6d1dea769a390f07684c1e1612789d4745f5b8
Damian Orzechowski <damianmisiek110@gmail.com>
2020-11-05 21:44:46 +0100

    Css dodany

--- Projekt 2 Notepacket/main.js
+++ Projekt 2 Notepacket/main.js
@@ -1,85 +1,142 @@
-const lsKey = 'notes';
+const lsKey = 'notes'
 // 1. zapisywanie notatki i tablicy notatek w localStorage
-
+function saveNote (note) {
+  notes.push(note)
+  localStorage.setItem(lsKey, JSON.stringify(notes))
+}
 // notatka: title, content, colour, pinned, createDate
-const notes = [];
+const notes = []
 
 const note = {
-    title: 'pierwsza notatka',
-    content: 'pierwsza notatka',
-    colour: '#ff1280',
-    pinned: false,
-    createDate: new Date()
-};
-
-notes.push(note);
-notes.push(note);
-notes.push(note);
-
-localStorage.setItem(lsKey, JSON.stringify(notes));
-
-// 2. wczytywanie z localStorage
-const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsKey));
-console.log(notesFromLocalStorage);
-
-const mappedNotes = notesFromLocalStorage.map(note => {
-    note.createDate = new Date(note.createDate);
-    return note;
-});
-
-
-//3. modyfkowanie struktury htmla
-function showNotes() {
-    const notesContainer = document.querySelector('main');
-    // pierwszy sposób
-    // for(const note of mappedNotes) {
-    //     // console.log(note.createDate.toLocaleString());
-    //     const htmlNote = 
-    //         `<section class="note">
-    //             <h1>${note.title}</h1>
-    //             <p>${note.content}</p>
-    //             <h4>${note.createDate.toLocaleString()}</h4>
-    //             <button>usuń</button>
-    //         </section>`;
-    //     notesContainer.innerHTML += htmlNote;
-    // }
-
-
-
-    // drugi sposób
-    for (const note of mappedNotes) {
-        // console.log(note.createDate.toLocaleString());
-        const htmlNote = document.createElement('section');
-        const htmlTitle = document.createElement('h1');
-        const htmlContent = document.createElement('p');
-        const htmlDate = document.createElement('h4');
-        const htmlRemoveBtn = document.createElement('button');
-
-        htmlTitle.innerHTML = note.title;
-        htmlContent.innerHTML = note.content;
-        htmlDate.innerHTML = note.createDate.toLocaleString();
-        htmlRemoveBtn.innerHTML = 'usuń';
-
-        // htmlRemoveBtn.addEventListener()
-
-        htmlNote.classList.add('note');
-        htmlNote.appendChild(htmlTitle);
-        htmlNote.appendChild(htmlContent);
-        htmlNote.appendChild(htmlDate);
-        htmlNote.appendChild(htmlRemoveBtn);
-
-        notesContainer.appendChild(htmlNote);
-    }
+  title: '',
+  content: '',
+  colour: '#ff1280',
+  pinned: false,
+  createDate: ''
 }
 
+// 3. modyfkowanie struktury htmla-ładowanie po otwarciu strony/dodaniu nowej notatki
+document.querySelector('#shownotes').addEventListener('click', showNotes)
+function showNotes () {
+  const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsKey))
+  console.log(notesFromLocalStorage)
+
+  const mappedNotes = notesFromLocalStorage.map(note => {
+    note.createDate = new Date(note.createDate)
+    return note
+  })
+
+  const notesContainer = document.querySelector('main')
+  // usuwanie starych kontenerow przez zaladowaniem nowych
+  while (notesContainer.firstChild) {
+    notesContainer.removeChild(notesContainer.lastChild)
+  }
+
+  // drugi sposób
+  for (const note of mappedNotes) {
+    // console.log(note.createDate.toLocaleString());
+    const htmlNote = document.createElement('section')
+    const htmlTitle = document.createElement('h1')
+    const htmlContent = document.createElement('p')
+    const htmlDate = document.createElement('h4')
+    const htmlRemoveBtn = document.createElement('button')
+    const htmlPinnedBtn = document.createElement('button')
+
+    htmlTitle.innerHTML = note.title
+    htmlContent.innerHTML = note.content
+    htmlDate.innerHTML = note.createDate.toLocaleString()
+    htmlRemoveBtn.innerHTML = 'usuń'
+    if(note.pinned){
+    htmlPinnedBtn.innerHTML = 'odepnij'
+    }
+    else{
+    htmlPinnedBtn.innerHTML = 'przypnij'
+    }
+
+    htmlRemoveBtn.addEventListener('click', removeNote)
+    htmlPinnedBtn.addEventListener('click', pinnedNote)
+
+    htmlNote.classList.add('note')
+    htmlNote.appendChild(htmlTitle)
+    htmlNote.appendChild(htmlContent)
+    htmlNote.appendChild(htmlDate)
+    htmlNote.appendChild(htmlRemoveBtn)
+    htmlNote.appendChild(htmlPinnedBtn)
+
+    htmlNote.style.backgroundColor = note.colour
+
+    note.pinned ? notesContainer.prepend(htmlNote) : notesContainer.appendChild(htmlNote)
+  }
+}
+//funkcja przypięcia na początek 
+function pinnedNote (event) {
+  const elementDate = event.target.parentNode.querySelector('h4').innerHTML
+  const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsKey))
+
+  let testDate
+  notes.length = 0
+
+  notesFromLocalStorage.map(note => {
+    notes.push(note)
+  })
+
+  notes.forEach(note => {
+    testDate = new Date(note.createDate).toLocaleString()
+
+    if (elementDate === testDate) {
+      note.pinned ? note.pinned = false : note.pinned = true
+    }
+    localStorage.removeItem(lsKey)
+    localStorage.setItem(lsKey, JSON.stringify(notes))
+    
+
+  })
+  showNotes()
+}
+   
+
+//funkcja usunięcia  przycisku
+function removeNote (event) {
+  event.target.parentNode.remove()
+  const elementDate = event.target.parentNode.querySelector('h4').innerHTML
+  const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsKey))
+
+  let testDate
+  notes.length = 0
+  const newNotes = []
+
+  notesFromLocalStorage.map(note => {
+    notes.push(note)
+  })
+
+  notes.forEach(note => {
+    testDate = new Date(note.createDate).toLocaleString()
+
+    if (elementDate !== testDate) {
+      if (newNotes.includes(note) === false) {
+        newNotes.push(note)
+      }
+    }
+    console.log('nn notes after loop:', newNotes)
+    localStorage.removeItem(lsKey)
+    localStorage.setItem(lsKey, JSON.stringify(newNotes))
+  })
+}
 // usuwanie elementu ze struktury html
 // notesContainer.removeChild()
 
-// 4. pobieranie danych z formularzy
-document.querySelector('#noteAdd').addEventListener('click', onNewNote);
-
-function onNewNote() {
-    const title = document.querySelector('#noteTitle').value;
-    const content = document.querySelector('#noteContent').value;
-    console.log(title, content);
+// 4. pobieranie danych z formularzy-tworzenie nowej notataki
+document.querySelector('#noteAdd').addEventListener('click', onNewNote)
+// tworzy i zwraca nową notatke z zwartoscia podaną w formularzach
+function onNewNote () {
+  const title = document.querySelector('#noteTitle').value
+  const content = document.querySelector('#noteContent').value
+  const color = document.querySelector('#Color').value
+  console.log(title, content)
+  const newNote = Object.assign({}, note)
+  newNote.title = title
+  newNote.content = content
+  newNote.createDate = new Date()
+  newNote.colour = color
+  saveNote(newNote)
 }
