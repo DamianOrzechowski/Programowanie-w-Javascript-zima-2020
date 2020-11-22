e0b70ee575194ab3d0789ade06841a97bfd826d2
Damian Orzechowski <damianmisiek110@gmail.com>
2020-11-22 16:29:37 +0100

    kulka wpda do dziury

--- Projekt 3 Ball in the hole/main.js
+++ Projekt 3 Ball in the hole/main.js
@@ -14,8 +14,12 @@ function Start(){ //funkcja rozpoczęcia gry
 window.addEventListener('deviceorientation', orintacjaUrzadzenia);
 pozycjaStart();
 }
+function Koniec(){//funkcaj konca
+    
+    Start()
+}
 
-function pozycjaStart(){
+function pozycjaStart(){ //ustawienie pozycji startowej
 poY = Math.floor(Math.random()*window.innerHeight);
 poX = Math.floor(Math.random()*window.innerWidth);
 dziurapoX = Math.floor(Math.random()*window.innerWidth);
@@ -26,26 +30,29 @@ kula.style.left = poX + 'px';
 kula.style.top = poY + 'px';
 
 
+
 }
 
 function orintacjaUrzadzenia(e){
-console.log(e);
+
 prX=e.gamma/45
 prY=e.beta/45
 przesuwaniekuli();
-console.log(prX,prY);
+
 }
 function przesuwaniekuli(){ //poruszanie się kuli po planszy
     if(poX+prX<window.innerWidth-50 && poY+prX>0){  
         poX+=prX;
-        kula.style.left=poX+'px';     
-        console.log("działa");   
+        kula.style.left=poX+'px';        
     }
     if(poY+prY<window.innerHeight-50 && poY+prY>0){
         poY+=prY;
         kula.style.top=poY+'px';        
     }
-    
+    if(Math.abs((poY - dziurapoY)) < 6 && Math.abs((poX - dziurapoX)) <6){//sprawdzenie
+        console.log("koniec")
+        Koniec();
+    }
 
     
 }
