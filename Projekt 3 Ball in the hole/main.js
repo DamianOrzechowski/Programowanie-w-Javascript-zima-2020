bc523ca76a9af10c56e34abb3400560f1e9372f0
Damian Orzechowski <damianmisiek110@gmail.com>
2020-11-22 15:45:08 +0100

    dodano przycisk start

--- Projekt 3 Ball in the hole/main.js
+++ Projekt 3 Ball in the hole/main.js
@@ -1,10 +1,18 @@
 let kula = document.querySelector("#kula");
-let plansza = document.querySelector("#dziura");
-window.addEventListener('deviceorientation', orintacjaUrzadzenia);
+let dziura = document.querySelector("#dziura")
+let plansza = document.querySelector("#plansza");
+document.querySelector("#btnstart").addEventListener('click',Start);
+
 let prX =0; // prędkość X
 let prY =0; //prędkość Y
-let poX =20; // pozycja X
-let poY=20; //pozycja Y
+let poX =250; // pozycja X
+let poY=250; //pozycja Y
+let dziurapoX=20;
+let dziurapoY=20;
+function Start(){ //funkcja rozpoczęcia gry
+window.addEventListener('deviceorientation', orintacjaUrzadzenia);
+}
+
 function orintacjaUrzadzenia(e){
 console.log(e);
 prX=e.gamma/45
@@ -12,7 +20,7 @@ prY=e.beta/45
 przesuwaniekuli();
 console.log(prX,prY);
 }
-function przesuwaniekuli(){
+function przesuwaniekuli(){ //poruszanie się kuli po planszy
     if(poX+prX<window.innerWidth-50 && poY+prX>0){  
         poX+=prX;
         kula.style.left=poX+'px';     
@@ -22,4 +30,7 @@ function przesuwaniekuli(){
         poY+=prY;
         kula.style.top=poY+'px';        
     }
+    
+
+    
 }
