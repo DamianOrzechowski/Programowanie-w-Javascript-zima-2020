3e91fc4f43bc238fb27cbad1d0653094e9761dc4
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-09 15:47:53 +0100

    śnieżka spada

--- Projekt-Snow/main.js
+++ Projekt-Snow/main.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-for (let i = 0; i < 5; i++) {
+/*for (let i = 0; i < 5; i++) {
 var x = Math.random() * window.innerWidth;
 var y = Math.random() * window.innerHeight;
 var size = Math.random()*8;
@@ -15,5 +15,26 @@ c.arc(x,y,size,0,Math.PI * 2,false);
 c.fillStyle = "white";
 //wypełnienie śnieżki na biało 
 c.fill();
+}*/
+//funkacja która będzie animować
+var y = 0;
+var size = Math.random()*8;
+var x = 10;
+function anime(){
+    requestAnimationFrame(anime)
+    console.log("dziala")
+    c.clearRect(0,0,innerWidth,innerHeight)
+    //var x = Math.random() * window.innerWidth;
+    
+    
+    c.beginPath();
+    //c.arc(x,0,5,0,Math.PI * 2,false); - wysztkie kulki są u góry 
+    c.arc(x,y,size,0,Math.PI * 2,false);
+    c.fillStyle = "white";
+    //wypełnienie śnieżki na biało 
+    c.fill();
+    //y to prędkość 
+    y+=5;
 }
+anime();
 
