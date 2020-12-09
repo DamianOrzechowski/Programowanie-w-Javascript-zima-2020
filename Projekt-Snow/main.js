6928045513ef50cfdc51e3451aeab7a7c341770a
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-09 16:50:00 +0100

    randomowy start snow flake

--- Projekt-Snow/main.js
+++ Projekt-Snow/main.js
@@ -1,25 +1,15 @@
 //pobranie canvas
-var canvas = document.querySelector('#canvas')
+let canvas = document.querySelector('#canvas')
 //ustawienie ramki 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext('2d');
-/*for (let i = 0; i < 5; i++) {
-var x = Math.random() * window.innerWidth;
-var y = Math.random() * window.innerHeight;
-var size = Math.random()*8;
-c.beginPath();
-//c.arc(x,0,5,0,Math.PI * 2,false); - wysztkie kulki są u góry 
-c.arc(x,y,size,0,Math.PI * 2,false);
-c.fillStyle = "white";
-//wypełnienie śnieżki na biało 
-c.fill();
-}*/
-//funkacja która będzie animować
-var y = 0;
-var size = Math.random()*8;
-var x = 10;
+let c = canvas.getContext('2d');
+
+let y = 0;
+let size = (Math.random()*8)+1;
+//randomowy start 
+let x = Math.random()*window.innerWidth;
 function anime(){
     requestAnimationFrame(anime)
     console.log("dziala")
@@ -37,4 +27,3 @@ function anime(){
     y+=5;
 }
 anime();
-
