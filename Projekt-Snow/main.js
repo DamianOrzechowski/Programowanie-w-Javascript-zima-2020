85168f9878351d16270da0ab924c33b1919ab450
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-09 13:33:27 +0100

    Śnieżki się pojawiają

--- Projekt-Snow/main.js
+++ Projekt-Snow/main.js
@@ -3,3 +3,16 @@ var canvas = document.querySelector('#canvas')
 //ustawienie ramki 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+for (let i = 0; i < 5; i++) {
+var x = Math.random() * window.innerWidth;
+var y = Math.random() * window.innerHeight;
+c.beginPath();
+//c.arc(x,0,5,0,Math.PI * 2,false); - wysztkie kulki są u góry 
+c.arc(x,y,5,0,Math.PI * 2,false);
+c.fillStyle = "white";
+//wypełnienie śnieżki 
+c.fill();
+}
+
