12490a513996583331c6b167fe20d86f88dbf145
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-09 15:27:22 +0100

    zmiana wielkości

--- Projekt-Snow/main.js
+++ Projekt-Snow/main.js
@@ -8,11 +8,12 @@ var c = canvas.getContext('2d');
 for (let i = 0; i < 5; i++) {
 var x = Math.random() * window.innerWidth;
 var y = Math.random() * window.innerHeight;
+var size = Math.random()*8;
 c.beginPath();
 //c.arc(x,0,5,0,Math.PI * 2,false); - wysztkie kulki są u góry 
-c.arc(x,y,5,0,Math.PI * 2,false);
+c.arc(x,y,size,0,Math.PI * 2,false);
 c.fillStyle = "white";
-//wypełnienie śnieżki 
+//wypełnienie śnieżki na biało 
 c.fill();
 }
 
