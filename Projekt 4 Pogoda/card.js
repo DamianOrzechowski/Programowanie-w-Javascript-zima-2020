fa46f7d4864bac0078fc844b41a07443b1ee1df5
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-16 11:40:42 +0100

    Start

--- Projekt 4 Pogoda/card.js
+++ Projekt 4 Pogoda/card.js
@@ -1,22 +1,21 @@
 
 const myKey = '6e0b2bd7dda1f02a5eb95ce5a13cf5c3'
-class  Card {
-    constructor(cityName) {
-        this.City = cityName ,
-        this.Temperature = '12'
-        
-    }  
-    fetchData() {
-    Promise((resolve ,reject) => {
-    fetch(`pro.openweathermap.org/data/2.5/forecast/hourly?q=${this.City}&appid=${myKey}`)
-    .then(response => response.json()
-      .then(
-        this.Temperature = response.list[0].main.temp
-      ))
-  resolve('sucess')
-    })
-            
-    }
+class Card {
+  constructor (cityName) {
+    this.City = cityName
+    this.Temperature = '000'
+  }
 
+  fetchData () {
+    Promise((resolve, reject) => {
+      fetch(`pro.openweathermap.org/data/2.5/forecast/hourly?q=${this.City}&appid=${myKey}`)
+        .then(response => response.json()
+          .then(
+            this.Temperature = response.list[0].main.temp
+          ))
+      resolve('sucess')
+    })
+  }
 }
-export default card
+
+export default Card
--- Projekt 4 Pogoda/ui.js
+++ Projekt 4 Pogoda/ui.js
@@ -1,33 +1,35 @@
 class UI {
-    constructor(container){
-        this.container = this.getCardContainer(container)
+    constructor (container) {
+      this.container = this.getCardContainer(container)
     }
-
-    getCardContainer(container) {
-        return document.querySelector(container)
+  
+    getCardContainer (container) {
+      return document.querySelector(container)
     }
-
-    createCard (Weathercard) {
-        const htmlCard = document.createElement('div')
-        htmlCard.classList.add('card')
-        const htmlCity = document.createElement('h1')
-        const htmlTemperature = document.createElement('p')
-        // const htmlWeather = document.createElement('h4') DODAC POZNIEJ
-        const htmlRemoveBtn = document.createElement('button')
-
-        htmlCity.innerHTML = card.City
-        htmlTemperature.innerHTML = card.Temperature
-        htmlRemoveBtn.innerHTML = 'Usuń'
-
-        htmlCard.appendChild(htmlCity)
-        htmlCard.appendChild(htmlTemperature)
-        htmlCard.appendChild(htmlRemoveBtn)
-        return htmlCard
+  
+    createCard (card) {
+      const htmlCard = document.createElement('div')
+      htmlCard.classList.add('card')
+      const htmlCity = document.createElement('h1')
+      const htmlTemperature = document.createElement('p')
+      // const htmlWeather = document.createElement('h4') DODAC POZNIEJ
+      const htmlRemoveBtn = document.createElement('button')
+  
+      htmlCity.innerHTML = card.City
+      htmlTemperature.innerHTML = card.Temperature
+      htmlRemoveBtn.innerHTML = 'Usuń'
+  
+      htmlCard.appendChild(htmlCity)
+      htmlCard.appendChild(htmlTemperature)
+      htmlCard.appendChild(htmlRemoveBtn)
+      return htmlCard
     }
+  
     addCard (card) {
-        const htmlCard = this.createCard(card)
-        console.log('container', this.notesContainer)
-        this.container.appendChild(htmlCard)
-      }
-}
-export default ui
+      const htmlCard = this.createCard(card)
+      console.log('container', this.notesContainer)
+      this.container.appendChild(htmlCard)
+    }
+  }
+  
+  export default UI
