b8078792fb7c86414e4a073db46dda4324586dd1
Damian Orzechowski <damianmisiek110@gmail.com>
2020-12-16 11:21:46 +0100

    Card

--- Projekt 4 Pogoda/card.js
+++ Projekt 4 Pogoda/card.js
@@ -1,13 +1,22 @@
 
+const myKey = '6e0b2bd7dda1f02a5eb95ce5a13cf5c3'
 class wehatercard {
-    constructor() {
-        this.City = cityName 
+    constructor(cityName) {
+        this.City = cityName ,
+        this.Temperature = '12'
         
     }  
     fetchData() {
-    const fetchPromise = new Promise(resolve ,reject) => {
-    const respose= fetch() 
+    Promise((resolve ,reject) => {
+    fetch(`pro.openweathermap.org/data/2.5/forecast/hourly?q=${this.City}&appid=${myKey}`)
+    .then(response => response.json()
+      .then(
+        this.Temperature = response.list[0].main.temp
+      ))
+  resolve('sucess')
+    })
             
     }
 
 }
+export default Card
