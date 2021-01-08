c234029b8fd6c2d27b8f4679421ff5657589cded
Damian Orzechowski <damianmisiek110@gmail.com>
2021-01-08 22:14:52 +0100

    Poprawiony ccs drumkid

--- Projekt 1 -Sound Kit przerobiony/percussion.js
+++ Projekt 1 -Sound Kit przerobiony/percussion.js
@@ -1,9 +1,7 @@
 function Percussion (SoundId) {
    this.SoundId = SoundId;
    this.soundDate = Date.now()
-    this.Test =() => {
-      console.log('działa')
-    }
+    
     this.PlaySound =() => {
       const sound = document.querySelector('#' + SoundId);
       sound.play();
