ecb6830f155cc72cf22519a2ed9dbca35af23b4b
Damian Orzechowski <damianmisiek110@gmail.com>
2021-01-12 19:05:09 +0100

    Es lint

--- Projekt-Snow/main.js
+++ Projekt-Snow/main.js
@@ -1,70 +1,70 @@
 //pobranie canvas
 let canvas = document.querySelector('#canvas')
 //ustawienie ramki 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-let c = canvas.getContext('2d');
+let c = canvas.getContext('2d')
 
 function SnowFlake(x,y,speed,size){
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.size = size;
+    this.x = x
+    this.y = y
+    this.speed = speed
+    this.size = size
 
     //funkcja pojawienia się śnieżki 
     this.letitsnow = function(){
-     c.beginPath();
-     c.arc(this.x,this.y,this.size,0,Math.PI*2,false);
-     c.fillStyle ="white";
-     c.fill();
+        c.beginPath()
+        c.arc(this.x,this.y,this.size,0,Math.PI*2,false)
+        c.fillStyle ='white'
+        c.fill()
     }
     //funkcja zapętlenia śnieżek
     this.snowLoop = function(){
         if(this.y > innerHeight){
-            this.x= Math.random()*window.innerWidth;
-            this.y=-10;
+            this.x= Math.random()*window.innerWidth
+            this.y=-10
         }
-        this.y += this.speed;
-        this.letitsnow();
+        this.y += this.speed
+        this.letitsnow()
     }
 
 }
 
-let snowArray = [];
+let snowArray = []
 for(let i = 0; i <150;i++){
-let x = Math.random()*innerWidth;
-let y = -10;
-//let speed = Math.random()*10;
-let size = Math.floor(Math.random()*4)+1;
-let speed;
-switch(size){
-case 1:
-speed=1+Math.random()*1.5;
-break;
-case 2:
- speed=1.6+(Math.random()*1.8);
-break;
-case 3:
-    speed=1.9+(Math.random()*2.2);
-break;
-case 4:
-    speed=2.5+(Math.random()*3);
-break;
-}
+    let x = Math.random()*innerWidth
+    let y = -10
+    //let speed = Math.random()*10;
+    let size = Math.floor(Math.random()*4)+1
+    let speed
+    switch(size){
+    case 1:
+        speed=1+Math.random()*1.5
+        break
+    case 2:
+        speed=1.6+(Math.random()*1.8)
+        break
+    case 3:
+        speed=1.9+(Math.random()*2.2)
+        break
+    case 4:
+        speed=2.5+(Math.random()*3)
+        break
+    }
 
-snowArray.push(new SnowFlake(x,y,speed,size));
+    snowArray.push(new SnowFlake(x,y,speed,size))
 }
 
 function animateSnow(){
-    requestAnimationFrame(animateSnow);
-    c.clearRect(0,0,innerWidth,innerHeight);
+    requestAnimationFrame(animateSnow)
+    c.clearRect(0,0,innerWidth,innerHeight)
     for (let i = 0; i < snowArray.length;i++){
-        snowArray[i].snowLoop();
+        snowArray[i].snowLoop()
     }
 
 }
-animateSnow();
+animateSnow()
 
 
 
