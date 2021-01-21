b21f4956f08ba794d2ee825ddc32631dcea272fb
Damian Orzechowski <damianmisiek110@gmail.com>
2021-01-21 11:15:22 +0100

    ostatnie

--- Ćw 1 lightbox/main.js
+++ Ćw 1 lightbox/main.js
@@ -9,27 +9,27 @@ let nextEl// poprzedni element danego zjecia
 
 close.addEventListener('click', hideLightbox)// zamykanie lighboxa
 function hideLightbox () {
-  lightbox.classList.remove('visible')
+    lightbox.classList.remove('visible')
 }
 
 for (let index = 0; index < images.length; index++) {
-  const img = images[index]
-  img.addEventListener('click', showLightbox)
+    const img = images[index]
+    img.addEventListener('click', showLightbox)
 }
 
 function showLightbox (ev) { // otwieranie lightboxa
-  const lightboxImg = document.querySelector('.lightbox img')
-  lightboxImg.src = ev.target.src
-  lightbox.classList.add('visible')
-  prevEl = ev.target.previousElementSibling
-  nextEl = ev.target.nextElementSibling
+    const lightboxImg = document.querySelector('.lightbox img')
+    lightboxImg.src = ev.target.src
+    lightbox.classList.add('visible')
+    prevEl = ev.target.previousElementSibling
+    nextEl = ev.target.nextElementSibling
 }
 next.addEventListener('click', function () { // przwijanie zdjec do przodu
-  hideLightbox()
-  nextEl ? nextEl.click() : images[0].click()// sprawdzenie czy obecne zdjecie jest ostatnim  w kolejnosci, jesli tak to nastepuje zawiniecie
+    hideLightbox()
+    nextEl ? nextEl.click() : images[0].click()// sprawdzenie czy obecne zdjecie jest ostatnim  w kolejnosci, jesli tak to nastepuje zawiniecie
 })
 
 prev.addEventListener('click', function () {
-  hideLightbox()
-  prevEl ? prevEl.click() : images[images.length - 1].click()// sprawdzenie czy obecne zdjecie jest pierwszym  w kolejnosci, jesli tak to nastepuje zawiniecie
+    hideLightbox()
+    prevEl ? prevEl.click() : images[images.length - 1].click()// sprawdzenie czy obecne zdjecie jest pierwszym  w kolejnosci, jesli tak to nastepuje zawiniecie
 })
